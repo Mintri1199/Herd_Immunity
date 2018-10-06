8d4ec231f82d53a5abc52a22843c384137297c96
Mintri1199 <Mintri1199@github.com>
2018-10-06 15:23:40 -0700

    comments added

--- person_test.py
+++ person_test.py
@@ -4,12 +4,12 @@ import unittest
 
 
 class PersonTest(unittest.TestCase):
-
+    # Set up the virus objects for tests
     def setUp(self):
         self.weak_virus = Virus("Weak", 0, 0)
-        self.deadly_virus = Virus("Deadly", 0.99, 0.99)
+        self.deadly_virus = Virus("Deadly", 1, 1)
 
-    # Checking the instantiation of person object
+    # Checking the instantiation of person object with virus
     def test_person_instantiation(self):
         new_person = Person(1, False, self.weak_virus)
         assert new_person.infection is self.weak_virus
@@ -17,17 +17,15 @@ class PersonTest(unittest.TestCase):
         assert new_person._id is 1
         assert new_person.is_vaccinated is False
 
+    # Check two cases of person objects
     def test_did_survive_method(self):
         dying_person = Person(1, False, self.deadly_virus)
         healthy_person = Person(2, False, self.weak_virus)
-
 
         assert dying_person.did_survive_infection() is False
         assert dying_person.is_alive is False
         assert dying_person.infection is not None
 
-
         assert healthy_person.did_survive_infection() is True
         assert healthy_person.is_alive is True
-        # Check if infection equal to None after surviving
         assert healthy_person.infection is None
