006881009b1b603691fe2f8ed5b8e4d86a547a5c
Mintri1199 <Mintri1199@github.com>
2018-10-06 16:46:46 -0700

    Logger finished

--- logger.py
+++ logger.py
@@ -1,9 +1,9 @@
 class Logger(object):
 
     def __init__(self, file_name):
-
         self.file_name = file_name
 
+    # Write the First line of the file
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
                        basic_repro_num):
         first_line = str(("{} {} {} {} {}\n".format(pop_size, vacc_percentage,
@@ -25,8 +25,16 @@ class Logger(object):
                 else:
                     file.write("{} fails to infect {}\n".format(person1._id, person2._id))
 
-    def log_infection_survival(self, person, did_die_from_infection):
-        pass
+    # Record whether the person die or survived from the infection
+    def log_infection_survival(self, person=object, did_die_from_infection=bool):
+        with open(self.file_name, "a") as file:
+            if did_die_from_infection:
+                file.write("{} died from the infection\n".format(person._id))
+            else:
+                file.write("{} survived the infection\n".format(person._id))
 
+    # Record the number of cycles has happened in the simulation
     def log_time_step(self, time_step_number):
-        pass
+        with open(self.file_name, "a") as file:
+            file.write("Time step {} ended, beginning {}".format(time_step_number, time_step_number + 1))
+
